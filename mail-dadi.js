ec2b54e789065b3be84b9a4d98939d21ae03abdd
Driccioli <driccioli53@gmail.com>
2021-05-31 15:46:46 +0200

    Aggiunti commenti

--- mail-dadi.js
+++ mail-dadi.js
@@ -2,6 +2,7 @@ var inputName = prompt("Inserisci il tuo nome.")
 var allowed = false;
 var allowedNames =["Ottavio", "Alfredo", "Davide"];
 
+// Check se il nome inserito può accedere al gioco 
 for(i=0; i<3; i++){
     if(inputName === allowedNames[i]){
         allowed = true;
@@ -9,17 +10,20 @@ for(i=0; i<3; i++){
 }
 
 if(allowed == true){
+    // Calcolo numeri dadi e stampa su schermo 
     var playerNum = Math.floor(Math.random() * 6) + 1;
     var computerNum = Math.floor(Math.random() * 6) + 1;
 
     document.getElementById("playerNum").innerHTML ="Risultato giocatore: " + playerNum;
     document.getElementById("computerNum").innerHTML ="Risultato coumputer: " + computerNum;
 
+    // Dichiarazione risultato 
     if(playerNum > computerNum){
         document.getElementById("result").innerHTML = "Hai vinto ai dadi! Congratulazioni!"
     }   else{
         document.getElementById("result").innerHTML = "Hai perso. Peccato!"
     }
 }   else{
+    // L'utente inserito non può entrare 
     document.getElementById("result").innerHTML = "Ci dispiace, non sei autorizzato a giocare ai dadi."
 }
